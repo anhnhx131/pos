c3a249bf603823583dd709729c7f7702ad9d3633
anhnh131 <hoanganh.hn0301@gmail.com>
2025-07-29 15:37:38 +0700

    feat: update

--- start.sh
+++ start.sh
@@ -165,6 +165,7 @@ for (( i=0; i<$NUM_NODES; i++ )); do
       --wallet-dir=/data/wallet \
       --wallet-password-file=/data/wallet/password.txt \
       --graffiti="VALIDATOR-$i"
+      --suggested-fee-recipient=0x23081455D3FEaf17426176dfc5Ee7A3ce519aD33
   fi
 
 done
