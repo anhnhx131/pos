ca1b0f9f01b66afe85c41448eef1f6231f31ea28
anhnh131 <hoanganh.hn0301@gmail.com>
2025-07-30 16:35:37 +0700

    update

--- upgrade.sh
+++ upgrade.sh
@@ -13,10 +13,10 @@ for (( i=0; i<$NUM_NODES; i++ )); do
   # validator
   VALIDATOR_NODE_IP="10.7.3.$((i+2))"
   VALIDATOR_NODE_NAME="pos_node$i-validator"
+  VALIDATOR_INDEX=$((i-2))
 
   # Run validator node
   if [ "$i" -ne 0 ]; then
-    VALIDATOR_INDEX=$((i-1))
     # remove container
     docker stop $VALIDATOR_NODE_NAME
     # remove container
@@ -38,7 +38,7 @@ for (( i=0; i<$NUM_NODES; i++ )); do
     gcr.io/prysmaticlabs/prysm/beacon-chain:v3.2.0 \
     --datadir=/data/beacondata \
     --min-sync-peers=1 \
-    --bootstrap-node=enr:-MK4QByMttvazzFwFJbNcq0z2X3MR3Iv6v8eEUbxUuHSW5DLdhTKIWjXMikBPpDS5Cf9hKAj5M_gmt-Uxpj-XncmRDqGAZg8_SFhh2F0dG5ldHOIAAAAAAAAAACEZXRoMpBOXjq3AQAAhAEAAAAAAAAAgmlkgnY0gmlwhAoHAgKJc2VjcDI1NmsxoQJZJFLCdVOkj35zGdm8bpM_AN2a8g_a4GWoXwTHOBP_XYhzeW5jbmV0cwCDdGNwgjLIg3VkcIIu4A,enr:-MK4QGXtR6S1Odtr0XfkY8uC-KMVQFeCfmy1nYU9wM6pUxi5P5N2aZmk-jj-6CELnbZVfJAZOtxkwfUadUOwTK7-86GGAZhAgYHfh2F0dG5ldHOIAAAAAAAAAACEZXRoMpBOXjq3AQAAhAEAAAAAAAAAgmlkgnY0gmlwhAoHAgOJc2VjcDI1NmsxoQMVfz11FN-GckAkmtWMvBcTA30NYlkGIzjL9PRFs-HXvohzeW5jbmV0cwCDdGNwgjLIg3VkcIIu4A,enr:-MK4QNVIt3io_v1nX_TAjyI0i3w2O7RcTuHDcZZemrsboR5bOjQHtEy7vb_f2F_E6uZcaF9anm6AI0IY0u7ffHcPo-uGAZhAgYPmh2F0dG5ldHOIAAAAAAAAAACEZXRoMpBOXjq3AQAAhAEAAAAAAAAAgmlkgnY0gmlwhAoHAgaJc2VjcDI1NmsxoQLHL6w4dBmXkEd4zcoZrqyQ019Vkp_U6MU8we0Wf556zohzeW5jbmV0cwCDdGNwgjLIg3VkcIIu4A \
+    --bootstrap-node=enr:-MK4QNEUb1iPB3uijEwaIcGV5DgEeMDGSkUiSbuf92cWUO3vZLSVoamK0byi7FjhvBIUYYIWTjhPIsbsAA_37H1k-VaGAZhanexNh2F0dG5ldHOIAAAAAAAAAACEZXRoMpC2eNdHAQAAhAEAAAAAAAAAgmlkgnY0gmlwhAoHAgKJc2VjcDI1NmsxoQJZJFLCdVOkj35zGdm8bpM_AN2a8g_a4GWoXwTHOBP_XYhzeW5jbmV0cwCDdGNwgjLIg3VkcIIu4A,enr:-MK4QE1ufdj0_mtQqFV54k4UsrRZ9HNGRImX05aQqTQp2mGiUZOlolyxcDhWB1cfjT_imXgfB15_4OWZuCQrMUfxKfqGAZhane3Kh2F0dG5ldHOIAAAAAAAAAACEZXRoMpC2eNdHAQAAhAEAAAAAAAAAgmlkgnY0gmlwhAoHAgOJc2VjcDI1NmsxoQLloM8VukrmSVDetKjK1tdPhqWcNs3A-uX7Lyu-yNSyD4hzeW5jbmV0cwCDdGNwgjLIg3VkcIIu4A \
     --chain-config-file=/config/config.yaml \
     --chain-id=84 \
     --network-id=84 \
@@ -58,9 +58,6 @@ for (( i=0; i<$NUM_NODES; i++ )); do
     $([ "$i" -eq 0 ] || [ "$i" -eq 1 ] && echo "--p2p-priv-key=/bn/privkey$i" || echo "")
 
     if [ "$i" -gt 1 ]; then
-      VALIDATOR_INDEX=$((i-1))
-      # sleep
-      sleep 300
       # run validator client
       docker run -d \
         --name $VALIDATOR_NODE_NAME \
@@ -77,6 +74,6 @@ for (( i=0; i<$NUM_NODES; i++ )); do
         --wallet-password-file=/data/wallet/password.txt
     fi
 
-  sleep 30
+  sleep 15
 
 done
