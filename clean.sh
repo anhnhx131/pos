ce2ce581364f10f8d1d706b9bb8838b061229afd
anhnh131 <hoanganh.hn0301@gmail.com>
2025-10-03 07:54:09 +0700

    update

--- clean.sh
+++ clean.sh
@@ -7,6 +7,8 @@ rm -Rf $SCRIPT_DIR/el/geth/.ethereum*
 # revert genesis.json
 echo "Revert genesis.json..."
 sed -i "s/\"shanghaiTime\": [0-9]*/\"shanghaiTime\": 9999999999999/g" el/geth/genesis.json
+sed -i "s/\"pragueTime\": [0-9]*/\"pragueTime\": 9999999999999/g" el/geth/genesis.json
+sed -i "s/\"cancunTime\": [0-9]*/\"cancunTime\": 9999999999999/g" el/geth/genesis.json
 
 # beacon node
 rm -Rf $SCRIPT_DIR/cl/node-*
