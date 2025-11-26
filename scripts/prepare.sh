0c2f638aa8b6c9362db47de5bfd9058cf1ace305
anhnh131 <hoanganh.hn0301@gmail.com>
2025-11-26 08:03:12 +0700

    update

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -853,26 +853,21 @@ EOF
     if [[ -n "${EL_SHANGHAI_TIME:-}" ]]; then
       jq_cmd="${jq_cmd} | .config.shanghaiTime = ${EL_SHANGHAI_TIME}"
     fi
-    
-    # Add cancunTime if set
-    if [[ -n "${EL_CANCUN_TIME:-}" ]]; then
-      jq_cmd="${jq_cmd} | .config.cancunTime = ${EL_CANCUN_TIME}"
-    fi
-    
+
     # Add pragueTime if set
     if [[ -n "${EL_PRAGUE_TIME:-}" ]]; then
       jq_cmd="${jq_cmd} | .config.pragueTime = ${EL_PRAGUE_TIME}"
     fi
     
-    # Add blobSchedule.cancun if all cancun blob configs are set
-    if [[ -n "${EL_BLOB_CANCUN_TARGET:-}" && -n "${EL_BLOB_CANCUN_MAX:-}" && -n "${EL_BLOB_CANCUN_BASE_FEE_UPDATE_FRACTION:-}" ]]; then
+    # Add cancunTime if set
+    if [[ -n "${EL_CANCUN_TIME:-}" ]]; then
+      jq_cmd="${jq_cmd} | .config.cancunTime = ${EL_CANCUN_TIME}"
+      # Add blobSchedule.cancun if all cancun blob configs are set
       jq_cmd="${jq_cmd} | .config.blobSchedule.cancun.target = ${EL_BLOB_CANCUN_TARGET}"
       jq_cmd="${jq_cmd} | .config.blobSchedule.cancun.max = ${EL_BLOB_CANCUN_MAX}"
       jq_cmd="${jq_cmd} | .config.blobSchedule.cancun.baseFeeUpdateFraction = ${EL_BLOB_CANCUN_BASE_FEE_UPDATE_FRACTION}"
-    fi
-    
-    # Add blobSchedule.prague if all prague blob configs are set
-    if [[ -n "${EL_BLOB_PRAGUE_TARGET:-}" && -n "${EL_BLOB_PRAGUE_MAX:-}" && -n "${EL_BLOB_PRAGUE_BASE_FEE_UPDATE_FRACTION:-}" ]]; then
+      
+      # Add blobSchedule.prague if all prague blob configs are set
       jq_cmd="${jq_cmd} | .config.blobSchedule.prague.target = ${EL_BLOB_PRAGUE_TARGET}"
       jq_cmd="${jq_cmd} | .config.blobSchedule.prague.max = ${EL_BLOB_PRAGUE_MAX}"
       jq_cmd="${jq_cmd} | .config.blobSchedule.prague.baseFeeUpdateFraction = ${EL_BLOB_PRAGUE_BASE_FEE_UPDATE_FRACTION}"
