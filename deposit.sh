c00b750151213a6175ead843787c85001add10bf
anhnh131 <hoanganh.hn0301@gmail.com>
2026-03-04 15:52:56 +0700

    update

--- deposit.sh
+++ deposit.sh
@@ -1,13 +1,35 @@
 # validators
 
 KEYSOTRES='[
-  {"pubkey": "b9d80028451da205ab2f8a0c8d8b72a840316e40179b02361cb8218734dca235e46e2ebb18d8e0fb7a7e804b2652da6f", "withdrawal_credentials": "010000000000000000000000b1ee8fc20d7ac4af4568aa46623a55ec31e0822a", "amount": 32000000000, "signature": "9046fe5bcbfcc49248310cbae11a29ef2c2726c7d6997c89fa05dfa6fc4c93adac9da90ed2d33fa38f1c921b1517fbf61456ecaa21f90c8ea2d604274f958cca4853ec15bf406667697d5742910effc5d57c298c127786c206fc5b59f06ca96f", "deposit_message_root": "bb2aba8dae99c1bc40d192e992d46e6af24bd8519e79503d8500d8b7316680ff", "deposit_data_root": "c225142e95dd56bbad7d8f8d65147370ade4df5bf26fe54e1018b4bc13785280", "fork_version": "00000084", "network_name": "joc", "deposit_cli_version": "1.2.2"},
-  {"pubkey": "967ea97033253dc385f41da8e270a1d75a641f38eda9e05fa4d8f28783a10a6b909bb033934db90f13584b0e7a34f092", "withdrawal_credentials": "010000000000000000000000b1ee8fc20d7ac4af4568aa46623a55ec31e0822a", "amount": 32000000000, "signature": "ab4560fe02b57c6a0b6ac25d7d7adba1d91887c7220534fca3a97696dc786636c5956a0f4ae20c9fb7b508bad2464bc30878845c082e1ae8f33457dcaa10a2c6d79416517535dc703284726451153606604137a9899292ecba9ffa9979b229b6", "deposit_message_root": "760ec8b9d136418231d3101f32cfd5a033f5112cd479ce9c188a40130be67aea", "deposit_data_root": "49465478b6df0dc448456e860dc51f8a7ec87d0af4c7f8db1d4faff497b8acc7", "fork_version": "00000084", "network_name": "joc", "deposit_cli_version": "1.2.2"}
+    {
+        "pubkey": "b428e5e4991b7cc27b0bb70735bdeb736bd95f01e05a2c4326b2eef9e452f478f7b24bb16c65f8d5720df911243e0523",
+        "withdrawal_credentials": "01000000000000000000000004b8a24201bea7aee98406ca25b2a03bab8f435c",
+        "amount": 32000000000,
+        "signature": "b81b1fe9a374818f04692fd01e35d939bba5a9a9033af88ff9883489db8bdc1ca71456a988db0cb60a6768d5f48c93c00d0e39a81410d51e0c2fee8c9e04cecabe87b645ff802e7c1f872b8b1a9f2fe891ddb2f8946b8cfa5cf661e877e94856",
+        "deposit_message_root": "893f2f01def1c933f4efcbc816994f0dfa0b64be0c416a18fe5c26d9cb45181b",
+        "deposit_data_root": "0324c00aa66dbb00587d5c65fa886baf360b7d86955ab3c3d178c6c2ebe7ffb8",
+        "fork_version": "00000051",
+        "network_name": "joc",
+        "deposit_cli_version": "1.2.2"
+    },
+     {
+        "pubkey": "a259cd3bd169a182833159eab8893317eeedf2970a20d36629745caef32c7ee50a363d6ece0a578e833cc42b7d97c7f3",
+        "withdrawal_credentials": "0100000000000000000000007ce5fea52a9c3b23251a8cabfbf0abab5c6e6c0d",
+        "amount": 32000000000,
+        "signature": "84ffacfb6dce1ce9bdccbea6d7d7205711985b07b370596e5fe8f5e2975d4b398e37492f8e5af50086af3f2242dd06a3011bc0fe95eadc39e38ae62685c26a70b67e41140e51d3f69402496137aad1b9d873031e40bb285e8b8ce8812b6a5b83",
+        "deposit_message_root": "cd927ea2e1e5b9e9ee2367fd124005857f12d769c47a2f6e93fca446762aa411",
+        "deposit_data_root": "0c536076ee79620f31911483cdaf040bb4e31a05c06fe0bbd622357e2d3ee009",
+        "fork_version": "00000051",
+        "network_name": "joc",
+        "deposit_cli_version": "1.2.2"
+    }
 ]'
 
 PRIVATE_KEY=0x152cca97c7be44efa3a405fc53f8019403ecf780bdfc14755334493f74d5e816
-RPC_URL=http://34.104.185.243:8545
-DEPOSIT_CONTRACT_ADDRESS=0x4242424242424242424242424242424242424242
+RPC_URL=http://18.138.249.132:8545/
+DEPOSIT_CONTRACT_ADDRESS=0xd2bee25f46ce2d92c868f3c07209675340039765
+
+# 10049
 
 # Map từng phần tử trong mảng thành các lệnh deposit
 echo "$KEYSOTRES" | jq -c '.[]' | while read -r item; do
